55aebc36aaf3d5875fd06ca1e6497c9130767be2
Crazy Technocrat <ameerbabu@iwex.in>
2024-10-09 22:06:05 +0530

    Update user.py

--- iwapp_next/events/user.py
+++ iwapp_next/events/user.py
@@ -1,21 +1,29 @@
 import frappe
 from frappe import _
 
-
 def before_save(doc, method):
-    if doc.role_profile_name:
-        designation = frappe.db.get_value("Employee", {"user_id":doc.name, "status" : "Active"}, "designation")
-        if designation:
-            if doc.role_profile_name != designation:
-                frappe.throw(_("The <b>Role Profile</b> you selected is not matching with the <b>Designation</b> of the Employee."))
-            else:
-                doc.module_profile = doc.role_profile_name
+    if doc.is_new():
+        if doc.role_profile_name:
+            
+            designation = frappe.db.get_value("Employee", {"user_id": doc.name, "status": "Active"}, "designation")
+            if designation:
+              
+                if doc.role_profile_name != designation:
+                    frappe.throw(_("The <b>Role Profile</b> you selected is not matching with the <b>Designation</b> of the Employee."))
+                else:
+                    doc.module_profile = doc.role_profile_name
+
+               
+                if doc.module_profile:
+                    if doc.role_profile_name != doc.module_profile:
+                        frappe.throw(_("User's <b>Role Profile</b> and <b>Module Profile</b> should be the same."))
+        else:
+          
             if doc.module_profile:
-                if doc.role_profile_name != doc.module_profile:
-                    frappe.throw(_("User's <b>Role Profile</b> and <b>Module Profile</b> should be the same."))
+                doc.module_profile = ""
     else:
-        if doc.module_profile:
-            doc.module_profile = ""
+        pass
+
 
 def on_update(doc, method):
     if doc.role_profile_name and doc.module_profile and not doc.api_key:
